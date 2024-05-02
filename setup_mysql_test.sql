af440f2b9588009f977dde30ab4fd84092e1c480
Amaara <amara.alex96@gmail.com>
2024-05-02 03:24:07 +0000

    Mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
--- Makes preperation for MYSQL for this project
-CREATE DATABASED IF NOT EXISTS hbnb_test_db;
+-- Prepares MySQL for this project
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
